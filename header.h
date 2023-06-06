0e9fc108caf8f6a44f1a4e358d77ade5eea08a62
dorisvidovic <78934564+dorisvidovic@users.noreply.github.com>
2023-06-06 09:53:43 +0200

    Update header.h

--- header.h
+++ header.h
@@ -1,17 +1,44 @@
-#pragma once
-
-typedef struct {
-    char name[50];
-    char description[100];
-    float price;
-} Drink;
-
-typedef struct {
-    char name[50];
-    Drink drinks[10];
-    int numDrinks;
-} DrinkCategory;
-
-void displayMenu(DrinkCategory menu[], int numDrinkCategories);
-void displayCategory(DrinkCategory category);
-float getTotal(Drink order[], int orderSize);
+#pragma once 
+  
+#define MAX_USERS 10 
+  
+extern int numUsers;  // Declare the variable as extern 
+  
+typedef enum { 
+    ADMIN, 
+    MODERATOR, 
+    GUEST 
+} Authority; 
+  
+typedef struct { 
+    char username[50]; 
+    char password[50]; 
+    Authority authority; 
+} User; 
+  
+extern User users[MAX_USERS];  // Declare the users array as extern 
+  
+User users[MAX_USERS] = { 
+    {"admin", "admin123", ADMIN}, 
+    {"moderator", "mod123", MODERATOR}, 
+    {"guest", "guest123", GUEST} 
+}; 
+  
+int numUsers = sizeof(users) / sizeof(User); 
+  
+typedef struct { 
+    char name[50]; 
+    char description[100]; 
+    float price; 
+} Drink; 
+  
+typedef struct { 
+    char name[50]; 
+    Drink drinks[10]; 
+    int numDrinks; 
+} DrinkCategory; 
+  
+void displayMenu(DrinkCategory menu[], int numDrinkCategories); 
+void displayCategory(DrinkCategory category); 
+float getTotal(Drink order[], int orderSize); 
+ 
