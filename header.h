d806ba926ddfbb4e377e2fd7296286f91880f407
dorisvidovic <78934564+dorisvidovic@users.noreply.github.com>
2023-06-06 13:42:52 +0200

    Update header.h

--- header.h
+++ header.h
@@ -1,44 +1,64 @@
-#pragma once 
-  
-#define MAX_USERS 10 
-  
-extern int numUsers;  // Declare the variable as extern 
-  
-typedef enum { 
-    ADMIN, 
-    MODERATOR, 
-    GUEST 
-} Authority; 
-  
-typedef struct { 
-    char username[50]; 
-    char password[50]; 
-    Authority authority; 
-} User; 
-  
-extern User users[MAX_USERS];  // Declare the users array as extern 
-  
-User users[MAX_USERS] = { 
-    {"admin", "admin123", ADMIN}, 
-    {"moderator", "mod123", MODERATOR}, 
-    {"guest", "guest123", GUEST} 
-}; 
-  
-int numUsers = sizeof(users) / sizeof(User); 
-  
-typedef struct { 
-    char name[50]; 
-    char description[100]; 
-    float price; 
-} Drink; 
-  
-typedef struct { 
-    char name[50]; 
-    Drink drinks[10]; 
-    int numDrinks; 
-} DrinkCategory; 
-  
-void displayMenu(DrinkCategory menu[], int numDrinkCategories); 
-void displayCategory(DrinkCategory category); 
-float getTotal(Drink order[], int orderSize); 
- 
+#ifndef HEADER_H
+#define HEADER_H
+
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MAX_USERS 10
+#define MAX_ARTICLES 100
+#define MAX_DRINKS 10
+#define MAX_DRINK_CATEGORIES 10
+
+
+typedef enum {
+	ADMIN,
+	MODERATOR,
+	GUEST
+} Authority;
+
+typedef struct {
+	char name[50];
+	char description[100];
+	float price;
+} Drink;
+
+typedef struct {
+	char name[50];
+	Drink drinks[MAX_DRINKS];
+	int numDrinks;
+} DrinkCategory;
+
+// Typedef for DrinkCategory structure
+typedef DrinkCategory Category;
+
+extern Category menu[MAX_DRINK_CATEGORIES];
+extern int numDrinkCategories;
+
+typedef struct {
+	char username[50];
+	char password[50];
+	Authority authority;
+} User;
+
+extern User users[MAX_USERS];
+extern int numUsers;
+
+typedef struct {
+	char title[100];
+	char content[500];
+} Article;
+
+extern Article articles[MAX_ARTICLES];
+extern int numArticles;
+
+void displayMenu(Category* menu, int numDrinkCategories);
+void displayCategory(Category category);
+float getTotal(Drink* order, int orderSize);
+
+void addUser(const char* username, const char* password, Authority authority);
+User* loginUser(const char* username, const char* password);
+void addArticle(const char* title, const char* content, User* user);
+
+#endif
